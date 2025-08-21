0549a6193186eee1d51fe9497d5cd2230f8533c7
JWT890 <jwtaylor659@gmail.com>
2025-08-21 16:15:58 -0500

    update for now

---
+++ jobs.py
@@ -0,0 +1,36 @@
+import feedparser
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import threading
+import time
+import requests
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask(__name__)
+jobs_listings = []
+
+RSS_URLS = {
+    "Blue Origin": "www.indeed.com/jobs?q=blue+origin&l=huntsville%2C+al&from=subwaywebapp&radius=25"
+}
+
+def get_jobs(rss_url):
+    """Fetch jobs from a given RSS URL"""
+    feed = feedparser.parse(rss_url)
+    return [entry.title for entry in feed.entries]
+
+def update_job_listings():
+
+
+def schedule_fetch():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_job_listings, 'interval', hours=3)
+    scheduler.start()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', job_listings=jobs_listings)
+
+if __name__ == "__main__":
+    threading.Thread(target=schedule_fetch, daemon=True).start()
+    app.run(debug=True)
